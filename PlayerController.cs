4294f63b94118b6251e1fb3be91b640c147e4106
Navai Suleymanli <80199998+Navai-Suleymanli@users.noreply.github.com>
2022-06-05 17:06:41 +0400

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,63 +4,86 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody playerRb; // Creating Rigidbody type variable
-    private Animator playerAnim;  // creating animator variable
+    private Rigidbody playerRb;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    public float jumpForce = 15;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver;
+    public bool doubleJumpUsed = false;
+    public float doubleJumpForce;
+    public bool doubleSpeed = false;
 
-    public ParticleSystem explosionParticle;  // creating particle system variable
-    public ParticleSystem dirtParticle; // creating particle system variable
-    public AudioClip jumpSound; // created audioclip variable
-    public AudioClip crashSound; // created audioclip variable
-    private AudioSource playerAudio;  // added audiosource type
-    public float jumpForce = 30;  // how high it will jump
-    public float gravityModifier;  // gravitation modifier
-    public bool isOnGround = true;  // boolean variable
-    public bool gameOver = false;  // created gameover boolean component
-    
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>(); // adding rigidbody component to our playerRb
-        playerAnim = GetComponent<Animator>();  // adding animator to the variable
-        playerAudio = GetComponent<AudioSource>(); // added audiosource to the variable
-        // physics shows every physics element that is possible to add to our game
-        Physics.gravity *= gravityModifier;  // physics increases gravitymodifier times
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)  // when space is pressed, and isGround is true, and gameover is flase, set isground to false
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && gameOver == false)
         {
-            // AddForce adds force to our player
-            //after , you can apply different force modes. .impulse immediately applies the force, that we wanted to apply
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);  // jump and add impulse force
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
-            playerAnim.SetTrigger("Jump_trig");  // sets trigger componenet for the jumping animation
-            dirtParticle.Stop();  // it will stop the dirt particle because player will jumping
-            playerAudio.PlayOneShot(jumpSound, 1.0f);  // using playOneShat function, added jumpsound and set it as 1
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+
+            doubleJumpUsed = false;
         }
+        else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
+        {
+            doubleJumpUsed = true;
+            playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+            playerAnim.Play("Running_Jump", 3, 0f);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+
+        /* This piece of code will check if the user has pressed down the left shift. If they have, it will set the
+        doubleSpeed boolean to true, otherwise the boolean will be false. We also set the animator parameter
+        Speed_Multiplier according to if doubleSpeed is true or not. This parameter will be set up a bit later on.
+        */
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            doubleSpeed = true;
+            playerAnim.SetFloat("Speed_Multiplier", 2.0f);
+        }
+        else if (doubleSpeed)
+        {
+            doubleSpeed = false;
+            playerAnim.SetFloat("Speed_Multiplier", 1.0f);
+        }
+
     }
 
-    // it will detect if it is on the ground. and if it is, isOnGroung will be true.
-    public void OnCollisionEnter(Collision collision)   // created collison parameter
+    private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))   // if smt collides with ground tagged object
+        //tag yaradanda sonra bunu edirik
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            isOnGround = true;  // set isground to true
-            dirtParticle.Play();  // it will play the dirt particle because player will be running
+            isOnGround = true;
+            dirtParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
-            Debug.Log("Game Over!");
-            gameOver = true;  // set gameover to true
-            playerAnim.SetBool("Death_b", true);       // | setting variables for the death animation
-            playerAnim.SetInteger("DeathType_int", 1); // |  
-            explosionParticle.Play();  // it will play the explosion effect
-            dirtParticle.Stop();  // it will stop the dirt partcile, because game will be ended
-            playerAudio.PlayOneShot(crashSound, 1.0f);  // using playOneShat function, added jumpsound and set it as 1
+            Debug.Log("Game Over");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
-        
     }
 }
